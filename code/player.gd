0a7442d398385ee3336a10a184c44d898537af38
Masonzk <masonzkaplan@gmail.com>
2024-02-08 22:00:06 -0500

    please dont break

--- code/player.gd
+++ code/player.gd
@@ -1,24 +1,49 @@
 extends CharacterBody2D
 
-const ACCELERATION = 0.5
-const MAX_SPEED = 50
+const ACCELERATION = 6
+const MAX_SPEED = 75
 
-var direction = Vector2.ZERO
-var previous_direction = Vector2.ZERO
+var direction = 0
+var previous_direction = Vector2.DOWN
+
+var step_speed = 1
+
+enum STATE {
+	idle,
+	walk,
+	attack
+}
 
 func _physics_process(delta):
-	var horizontal = Input.get_action_strength("right") - Input.get_action_strength("left")
-	var vertical = Input.get_action_strength("down") - Input.get_action_strength("up")
+	if Input.is_action_just_pressed("down"):
+		direction = Vector2.DOWN
+	if Input.is_action_just_pressed("up"):
+		direction = Vector2.UP
+	if Input.is_action_just_pressed("right"):
+		direction = Vector2.RIGHT
+	if Input.is_action_just_pressed("left"):
+		direction = Vector2.LEFT
+	if !Input.is_action_pressed("left") & !Input.is_action_pressed("right") & !Input.is_action_pressed("up") & !Input.is_action_pressed("down"):
+		direction = Vector2.ZERO
 	
-	direction = Vector2(horizontal, vertical)
-	velocity.x = lerpf(velocity.x, MAX_SPEED*horizontal, ACCELERATION)
-	velocity.y = lerpf(velocity.y, MAX_SPEED*vertical, ACCELERATION)
+	velocity = direction.normalized() * MAX_SPEED
+	
+	#direction = Vector2(horizontal, vertical).normalized()
+	
+	#velocity = velocity.move_toward(direction*MAX_SPEED*step_speed, ACCELERATION)
 	
 	#velocity = velocity.normalized()
 	
-	$AnimationTree["parameters/BlendSpace2D/blend_position"] = direction
-	$AnimationTree["parameters/BlendSpace2D 2/blend_position"] = direction
+	#$AnimationTree["parameters/BlendSpace2D/blend_position"] = direction
+	#$AnimationTree["parameters/BlendSpace2D 2/blend_position"] = direction
 	
-	$AnimationTree["parameters/motion/blend_amount"] = int(direction == Vector2.ZERO)
+	#$AnimationTree["parameters/motion/blend_amount"] = int(direction == Vector2.ZERO)
 	
 	move_and_slide()
+
+
+func _on_walking_timeout():
+	if step_speed == 1:
+		step_speed = 0.75
+	elif step_speed == 0.75:
+		step_speed = 1
